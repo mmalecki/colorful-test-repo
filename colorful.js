0b076ff0e59494ea5ea4edd20a55496f184947a2
Maciej Małecki <maciej.malecki@notimplemented.org>
2011-11-28 11:58:57 +0100

    .

---
+++ colorful.js
@@ -0,0 +1,14 @@
+process.stdout.write('\033[32m');
+var N = 1024,
+    i = 0;
+setInterval(function () {
+  process.stdout.write('.');
+  if (i == 512) {
+    process.stdout.write('\033[31m');
+  }
+  else if (i == N) {
+    process.exit(0);
+  }
+  ++i;
+}, 40);
+
